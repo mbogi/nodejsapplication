a16a1efb0a2bb9f19c6a38ec56fba77ef85a7402
saitejakottapally <66955156+1919teja@users.noreply.github.com>
2021-11-15 14:54:32 +0530

    updated config

--- routes/Paytm/config.js
+++ routes/Paytm/config.js
@@ -1,6 +1,6 @@
 var PaytmConfig = {
-	mid: "fQ14",
-	key: "d9jHf%F",
+	mid: "fQfgpL84711357171944",
+	key: "dtdh_8O9f79jHf%F",
 	website: "WEBSTAGING",
 };
 module.exports.PaytmConfig = PaytmConfig;
